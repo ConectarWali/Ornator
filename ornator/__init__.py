b44dc89a20903b0b00734638363f1970e4655090
Conectar Wali SAS <167815566+ConectarWali@users.noreply.github.com>
2025-02-06 14:51:06 -0500

    Update __init__.py

--- ornator/__init__.py
+++ ornator/__init__.py
@@ -1,4 +1,5 @@
 from ornator.class_decorator import *
 from ornator.decorator import *
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+__all__ = ['AfterClassDecorator', 'AfterDecorator', 'BeforeClassDecorator', 'BeforeDecorator', 'DualClassDecorator', 'DualDecorator', 'EmptyClassDecorator', 'EmptyDecorator']
